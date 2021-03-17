62ce41b107f6dabbd5be237d441e028c6f73910e
jessicaUP <uphoff.jessica@gmail.com>
2021-03-17 16:00:55 -0400

    Update Stepable module with the moves method

--- Chess_part_1/stepable.rb
+++ Chess_part_1/stepable.rb
@@ -1,11 +1,37 @@
 module Stepable
 
     def moves(position)
-        moves = []
+        moves = [ ]  
+        moves = move_diff #call the method from piece classes
+
+        row = position[0]
+        column = position[1]
+
+        
+        moves_diff.each do |p_pos| #potential position 
+            new_row = p_pos[0]
+            new_column = p_pos[1]
+
+            new_pos = [ (row + new_row), (column + new_column) ]
+  
+
+
+
+
+
+
+    # current => while move diff > count
+    # 1 loop => [1,0] , -1
+    # i= 2
+
+        # posistion start => make one step =>
+        # board(add getter)
+        #4 place in 
+            # iterate through moves
         
     end
 
-    def move_diff
+    def move_diff  # assume this
         raise "move_diff not implemented"
     end
 
